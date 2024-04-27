5f0de622d49a7fb594e2e83d9469ad733c4f704f
SamiulHasan-dev <samiul.199897@gmail.com>
2024-04-27 18:32:30 +0600

    work on post

--- index.js
+++ index.js
@@ -29,12 +29,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // const craftCollection = client.db("craftDb").collection("craft");
+    const database = client.db("craftDb");
+    const craftCollection = database.collection("craft");
+
+
+
+    app.post('/crafts', async (req, res)=>{
+      const newCraft = req.body;
+      console.log(newCraft)
+      const result = await craftCollection.insertOne(newCraft);
+      res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
